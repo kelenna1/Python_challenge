d2471f0e46bd1b2c077fede18a29003ee6fb564d
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-09-29 21:05:07 +0100

    practiced more stuffs

---
+++ main.py
@@ -0,0 +1,14 @@
+import calculator
+x, y = 5, 3
+
+add_result = calculator.add(x, y)
+print(f"{x} + {y} = {add_result}")
+
+subtract_result = calculator.subtract(x, y)
+print(f"{x} - {y} = {subtract_result}")
+
+multiply_result = calculator.multiply(x, y)
+print(f"{x} * {y} = {multiply_result}")
+
+division_result = calculator.divide(x, y)
+print(f"{x} / {y} = {division_result}")
---
+++ python_3.py
@@ -0,0 +1,10 @@
+def main(number):
+    if number % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is not even it's odd")
+
+main(6)
+
+my_list = ["apple", "pineapple", "banana", "citrus"]
+print(my_list[0:3])
