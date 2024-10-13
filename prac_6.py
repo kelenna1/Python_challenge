651e60aee8716345b5d1c355c7c7300ffe0e97e9
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-10-13 17:55:04 +0100

    practice

--- prac_6.py
+++ prac_6.py
@@ -6,5 +6,10 @@ class Person:
     def student(self):
         return f"Name: {self.name}, Age: {self.age}"
     
+    def __del__(self):
+        print(f"{self.name} has been deleted")
+    
 school = Person("John", "22")
 print(f"{school.student()}")
+
+del school
