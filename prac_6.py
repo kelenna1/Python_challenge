9fda9b4dad1d7af2a56a6c66eedc103adff7d883
Okolie Emmanuel <leoemmanuel93@gmail.com>
2024-10-14 15:16:44 +0100

    prac_6

--- prac_6.py
+++ prac_6.py
@@ -13,3 +13,26 @@ school = Person("John", "22")
 print(f"{school.student()}")
 
 del school
+
+class Book:
+    def __init__(self, author, title, pages):
+        self.author = author
+        self.title = title
+        self.pages = pages 
+    
+    def __str__(self):
+        return f"{self.author} has a book titles {self.title} with about {self.pages} pages."
+
+    def __repr__(self):
+         return f"Book( title = {self.title}, author = {self.author}, pages = {self.pages})"
+    
+book = Book("Kelenna", "Franchise", 300)
+print(repr(book))
+           
+class Animal:
+    def __init__(self,eat,sleep):
+        self.eat = eat
+        self.sleep = sleep
+
+    def __str__(self):
+        return f""
